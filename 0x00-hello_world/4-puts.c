56e84ac2134accd995776f6679294df16693d492
Somtochi <mkparusomtochi26@gmail.com>
2022-09-08 13:29:41 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
 # include <string.h>
-
+/**
+ * main - this is where the program starts
+ * Return: 0
+ */
 int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
