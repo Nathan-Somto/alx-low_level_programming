6dcb3c552726982af4de921e5b1b22caf41487ed
Somtochi <mkparusomtochi26@gmail.com>
2022-09-08 13:47:42 -0700

    Task 5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main - where program starts
+ * Description: prints out a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
