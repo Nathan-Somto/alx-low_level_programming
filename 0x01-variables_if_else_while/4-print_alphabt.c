bf75f526e810e97e387aeb17539ff258e86f4917
Somtochi <mkparusomtochi26@gmail.com>
2022-09-09 16:28:14 -0700

    Task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+/**
+ * main - where program starts
+ * Description: print all lowercase
+ * alphabets apart from q and e
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' || alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+	}
+	return (0);
+}
