8c6d3131c2322e7c1bf4affe5af5396a5bbc171b
Somtochi <mkparusomtochi26@gmail.com>
2022-09-09 14:57:44 -0700

    Task 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+* main - where program starts
+* Description: print numbers
+* from 0 to 9
+* Return: 0
+*/
+int main(void)
+{
+int Number;
+for (Number = 0; Number <= 9; Number++)
+{
+	printf("%d", Number);
+}
+printf("\n");
+return (0);
+}
