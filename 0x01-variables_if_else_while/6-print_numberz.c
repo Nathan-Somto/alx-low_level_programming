7eadeb0358f5e653a2ba2a2f3574e004bb90a852
Somtochi <mkparusomtochi26@gmail.com>
2022-09-09 17:04:27 -0700

    Task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+ *  main - where program starts
+ *  Description: print numbers
+ *  from 0 to 9
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+}
