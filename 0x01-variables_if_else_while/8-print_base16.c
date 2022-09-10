88e35c1307af9be462f2e8b9ac708df8765c47a8
Somtochi <mkparusomtochi26@gmail.com>
2022-09-09 17:27:04 -0700

    Task 8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+/**
+ *  main - where program starts
+ *  Description: print
+ * hexadecimal numbers
+ * from 0 to f
+ *  Return: 0
+*/
+int main(void)
+{
+	int i;
+	char alphabet;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
