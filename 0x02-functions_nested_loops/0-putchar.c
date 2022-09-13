8be712dc8137233de9dfce94aff0604c1738d54e
Somtochi <mkparusomtochi26@gmail.com>
2022-09-13 11:59:58 -0700

    Task 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+# include "main.h"
+/**
+ *  main - where the program start
+ *  Return : 0
+*/
+int main(void)
+{
+
+	char *var = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(var[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
