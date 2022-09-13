0919e304b191cbe775754e7a6fc735b74bac673f
Somtochi <mkparusomtochi26@gmail.com>
2022-09-13 12:48:46 -0700

    Task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+ * print_alphabet_x10 - prints the
+ * lowercase letters 10 times
+ * Return: returns nothing
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
