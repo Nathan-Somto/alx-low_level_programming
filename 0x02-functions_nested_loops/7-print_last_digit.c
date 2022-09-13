9dda3afe02c883b796cbc2b9561990196ebb8295
Somtochi <mkparusomtochi26@gmail.com>
2022-09-13 14:50:10 -0700

    Task 7 done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,13 @@
 */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		return ((-1 * n) % 10);
+	int ld;
 
-	else
-		return (n % 10);
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	ld = n % 10;
+	_putchar(ld + '0');
+	return (ld);
 }
