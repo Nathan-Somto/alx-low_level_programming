3fa0ca9d1206d2ac78642c2cf54a36b1ce6297d0
Somtochi <mkparusomtochi26@gmail.com>
2022-09-13 19:27:44 -0700

    Task 8 done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,27 +6,19 @@
 */
 void jack_bauer(void)
 {
-	int i;
-	int j;
-	int k;
-	int l;
+	int h;
+	int m;
 
-	for (i = 0; i < 3; i++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (j = 0; j < 4; j++)
+		for (m = 0; m <= 59; m++)
 		{
-			for (k = 0; k < 6; k++)
-			{
-				for (l = 0; l < 10; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
-	}
+}
 }
