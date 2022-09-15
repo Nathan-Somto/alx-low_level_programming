0f8c2a9d35c9f38429cc52f929d003f6a7ba54db
Somtochi <mkparusomtochi26@gmail.com>
2022-09-15 16:30:11 -0700

    solved two questions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+# include "main.h"
+/**
+ * more_numbers - prints 0 to 14 using only _putchar
+ * Return:returns nothing
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (i >= 10)
+				_putchar((j / 10) + '0');
+			if (i >= 0)
+				_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
