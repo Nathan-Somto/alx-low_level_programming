c04d0fdd77701b6e83bac269dd7641999d189230
Somtochi <mkparusomtochi26@gmail.com>
2022-09-15 15:43:04 -0700

    Fizz buzz code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+/**
+ *  main - where program starts
+ *  Return: returns 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d ", i);
+
+		if (i != 100)
+			printf(" ");
+
+
+	}
+	return (0);
+}
