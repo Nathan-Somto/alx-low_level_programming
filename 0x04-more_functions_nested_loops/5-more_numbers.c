561a2bee1eb12571525052df3c4edaf4ee814988
Somtochi <mkparusomtochi26@gmail.com>
2022-09-15 16:38:15 -0700

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (i >= 10)
+			if (j >= 10)
 				_putchar((j / 10) + '0');
-			if (i >= 0)
-				_putchar((j % 10) + '0');
+			if (j >= 0)
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
