0042614ef7d923d00b81c3fadd3fff82df81887b
Somtochi <mkparusomtochi26@gmail.com>
2022-09-15 19:49:38 -0700

    task 7 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+# include "main.h"
+/**
+ * print_diagonal - prints a diagonal of a specified length
+ * @n: the length specified
+ * Return: Returns nothing
+*/
+void print_diagonal(int n)
+{
+	int i = 1;
+	int space;
+
+	while (1)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else if (i < n)
+		{
+			for (space = 0; space < i; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i += 1;
+
+		}
+		else
+			break;
+	}
+}
