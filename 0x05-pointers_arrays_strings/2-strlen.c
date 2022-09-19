177a266f2f4eb41ecc57355f45c3e7baee347cf0
Somtochi <mkparusomtochi26@gmail.com>
2022-09-19 05:26:09 -0700

    task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+# include "main.h"
+/**
+ * _strlen - gets the length of a string
+ * @s: the string passed
+ * Return: returns the length
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		*s++;
+		count++;
+
+	}
+	return (count);
+}
