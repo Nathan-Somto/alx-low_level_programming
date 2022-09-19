b99630304637d7339007aa875e6bf9fb6ffcd146
Somtochi <mkparusomtochi26@gmail.com>
2022-09-19 09:09:10 -0700

    task 8 done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * puts2 - prints only the even numbered elements of a string
+ * @str: string to be  used
+*/
+void puts2(char *str)
+{
+	int count;
+	int i;
+
+	for (count = 0;  *str != '\0'; str++)
+	{
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
+		if (i % 2 == 0)
+		{
+			printf("%c", *(str + i));
+		}
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void _puts(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
+void puts2(char *str);
