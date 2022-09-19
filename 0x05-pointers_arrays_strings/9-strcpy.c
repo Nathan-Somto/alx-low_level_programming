acc3b947c51070cb8e70bddb66223ea8d3e7728a
Somtochi <mkparusomtochi26@gmail.com>
2022-09-19 07:15:34 -0700

    Task 9 done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+# include  "main.h"
+/**
+ * _strcpy - copies the string
+ * @dest: destination for copying
+ * @src: source for copying
+ * Return: returns a string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return (*dest);
+}
