22aa8ccd55346ea7b1192adb7c583f3e7477e081
Somtochi <mkparusomtochi26@gmail.com>
2022-09-19 12:57:10 -0700

    task 5 done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * rev_string - returns a reversed string
+ * @s: the reversed string
+*/
+void rev_string(char *s)
+{
+	int i;
+	char *tmp;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = i - 1; j >= 0; j++)
+		tmp = s[j];
+	for (i = 0;  tmp[i] != '\0'; i++)
+		s[i] = tmp[i];
+
+
+}
