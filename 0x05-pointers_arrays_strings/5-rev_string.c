aaf45bef2877768f7aa3ba84e8d835dbed10221a
Somtochi <mkparusomtochi26@gmail.com>
2022-09-19 13:27:02 -0700

    task 5 done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,16 @@ void rev_string(char *s)
 	int i;
 	char tmp[1000];
 	int j;
+	int k = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		;
 	}
-	int k = 0;
 	for (j = i - 1; j >= 0; j++)
 	{
 		tmp[k] = s[j];
+		k++;
 	}
 	for (i = 0;  tmp[i] != '\0'; i++)
 		s[i] = tmp[i];
