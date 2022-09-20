271b0d8ff03ec20891154e39b1deb07d7f80eb29
Somtochi <mkparusomtochi26@gmail.com>
2022-09-19 20:05:40 -0700

    task 6 done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; *(str + i)  != '\0'; i += 2)
 	{
-		printf("%c", str[i]);
+		printf("%c", *(str + i));
 	}
 	printf("\n");
 }
