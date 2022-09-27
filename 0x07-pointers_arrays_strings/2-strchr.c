32e431273448808297260ff6361335a5943e95e6
Somtochi <mkparusomtochi26@gmail.com>
2022-09-26 18:21:23 -0700

    done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; 1; i++)
-	{
-		if (*(s + i) == c)
-			return (s + i);
-		if (*(s + i) == '\0')
-			return (NULL);
-	}
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
