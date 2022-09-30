f4d8853f863562c95a17b09a4b2b17c35553459e
Somtochi <mkparusomtochi26@gmail.com>
2022-09-30 07:18:54 -0700

    done task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * main - ....
+ * Return: ....
+ * @argc: ....
+ * @argv: ....
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
