9072258392c323118bafb2bd274c089c479d769a
Somtochi <mkparusomtochi26@gmail.com>
2022-09-30 13:16:28 -0700

    task 2 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# include <string..h>
+# include <stdlib.h>
+/**
+ * main - ....
+ * @argc: ...
+ * @argv: ...
+ * Return: ...
+*/
+int main(int argc, char **argv)
+{
+	int product = 1;
+	int i;
+	int value;
+
+	for (i = 1; i <= 2; i++)
+	{
+		if (argv[i] == '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		value = atoi(argv[i]);
+		product *= value;
+	}
+	printf("%d\n", product);
+	return (0);
+
+}
